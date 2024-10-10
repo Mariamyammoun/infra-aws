197d2aba383c8d266851be1b960fd06128ec6ebc
Mariam Yammoun <162291710+Mariamyammoun@users.noreply.github.com>
2024-10-10 19:22:27 +0100

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -83,7 +83,7 @@ resource "aws_instance" "my_server" {
   subnet_id              = aws_subnet.my_subnet.id
 
   tags = {
-    Name = "${var.name}-server"
+    Name = "${var.server_name}-server" 
   }
 
  
